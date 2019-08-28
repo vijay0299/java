cf1d72bdcc3b46a488a849f0035bb5c93a938288
vijay0299 <52507540+vijay0299@users.noreply.github.com>
2019-08-28 11:49:36 +0530

    Create Convert.java

---
+++ Convert.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Convert{
+public static void main(String[] args){
+try{
+String s="VIJAY";
+int i=Integer.parseInt(s);
+System.out.println(s.length());
+System.out.println(i);
+}catch(NumberFormatException v){System.out.println(v);}
+}
+}
